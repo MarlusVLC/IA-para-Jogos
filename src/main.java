cff7ce9721dfdce38fa7e6bcee33170ec6661fe8
Marlus Vinicius <marlusvinicius2014@outlook.com>
2020-09-10 12:07:38 -0300

    Adicionado um hashmap para iterar as acoes dos personagens

--- src/main.java
+++ src/main.java
@@ -1,14 +1,27 @@
+import java.util.HashMap;
 
 public class main {
 
     public static void main(String[] args){
 
-        Farmer bob = new Farmer(EnterMineAndDigForNugget.getInstance());
-        Farmer billy = new Farmer(LookAtTheWeather.getInstance());
+        HashMap<String, Farmer> farmers = new HashMap();
+        farmers.put("Bob", new Farmer(EnterMineAndDigForNugget.getInstance()));
+        farmers.put("Billy", new Farmer(LookAtTheWeather.getInstance()));
+
         while (true){
-            bob.ExecuteState();
-            billy.ExecuteState();
+            for (Farmer farmer : farmers.values()){
+                farmer.ExecuteState();
+            }
         }
+
+
+
+//        Farmer bob = new Farmer(EnterMineAndDigForNugget.getInstance())
+//        Farmer billy = new Farmer(LookAtTheWeather.getInstance());
+//        while (true){
+//            bob.ExecuteState();
+//            billy.ExecuteState();
+//        }
 
     }
 }
